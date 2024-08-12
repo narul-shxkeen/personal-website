00ab2c9a4a27b7117f7051bb8d3dfb56d9156aa5
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-08-12 10:29:26 +0530

    added some padding to projects section

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from "../assets/projectCard";
 
 function Projects() {
 
-return <div className="pt-20"> <div className="bg-slate-50 flex flex-col md:mt-28 mx-2 md:mx-4 rounded-xl py-4 md:py-8 px-4 gap-6">
+return <div className="pt-20"> <div className="bg-slate-50 flex flex-col md:mt-28 mx-4 md:mx-4 rounded-xl py-4 md:py-8 px-4 gap-6">
 <h1 className="poppins-semibold text-2xl md:text-4xl text-center">Projects</h1>
 <div className="grid grid-cols-1 md:grid-cols-3">
 <ProjectCard
